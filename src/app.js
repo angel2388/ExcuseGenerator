ecdb2abb1a11082f9ed06394a8c910b86c7b411f
angel2388 <rafa2388@gmail.com>
2022-09-22 18:59:01 +0000

    Excuse Generator

--- src/app.js
+++ src/app.js
@@ -1,11 +1,18 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
+  let who = ["the dog", "my granma", "his turtle", "my bird"];
+  let what = ["eat", "pissed", "crushed", "broked"];
+  let when = [
+    "before the class",
+    "right in time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+  let resultado =
+    who[Math.floor(Math.random() * who.length)] +
+    what[Math.floor(Math.random() * what.length)] +
+    when[Math.floor(Math.random() * when.length)];
+  console.log(resultado);
+  document.getElementById("excuse").innerHTML = resultado;
   console.log("Hello Rigo from the console!");
 };
